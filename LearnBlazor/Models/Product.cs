9170d2ca8d9d3e1d81ba1f4522c03d17c75e71da
jobin jose samuel <jobin.samuel@outlook.com>
2025-11-15 10:31:45 +0530

    One way data binding

---
+++ LearnBlazor/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearnBlazor.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public bool IsActive {  get; set; }
+    }
+}
