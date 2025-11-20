01a80ca8b76ef52c716d1fc93e625a3f1557e9c4
jobin jose samuel <jobin.samuel@outlook.com>
2025-11-20 13:52:06 +0530

    Toastr JS completed

---
+++ LearnBlazor/wwwroot/js/common.js
@@ -0,0 +1,10 @@
+﻿window.showToastr = function (type, message) {
+
+    if (type == "success") {
+        toastr.success(message);
+    }
+
+    if (type == "error") {
+        toastr.error(message);
+    }
+}
